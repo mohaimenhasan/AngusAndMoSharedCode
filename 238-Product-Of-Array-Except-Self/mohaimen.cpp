f4bbbf83f7964a85b8e05d87dfe2de6772f73af0
Mohaimen Khan <mkhan@bluecatnetwworks.com>
2018-06-22 09:02:43 -0400

    mohaimen failed the first question it doesn't work he's just playing dumb

---
+++ 238-Product-Of-Array-Except-Self/mohaimen.cpp
@@ -0,0 +1,62 @@
+/*
+Given an array nums of n integers where n > 1,
+return an array output such that output[i] is equal to the product of all the
+elements of nums except nums[i].
+
+Example:
+
+Input:  [1,2,3,4]
+Output: [24,12,8,6]
+Note: Please solve it without division and in O(n).
+
+Follow up:
+Could you solve it with constant space complexity?
+(The output array does not count as extra space for the purpose of space complexity analysis.)
+
+*/
+
+
+/*
+here's my idea:>
+
+dumb idea:
+
+try multiplying everything on the same place every time
+Time complexity would be out of the roof
+
+trying to outsmart:
+
+get the product once and then divide it by the index LOL 3smart5me
+however, what if I try multiplying by the receprocal of the number
+so like 2 -> 1/2
+
+basic idea:
+
+need a prcess with O(1) access
+so indeed a hash table maybe?
+
+1,2,3,4
+24,12,8,6
+
+
+*/
+
+
+
+
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+      vector <int> outputArray;
+      int product;
+      for (int i=0; i <nums.size(); i++){
+        product=product*nums[i];
+      }
+      for (int i=0; i < nums.size(); i++){
+        int out=product*1/nums[i];
+        outputArray.push(out);
+      }
+      return outputArray;
+    }
+};
