cea1c71a6c77771262ce697290daf6c61c4ba114
Mohaimen Khan <mkhan@bluecatnetworks.com>
2018-07-31 00:34:31 -0400

    refactored 520

--- 520. Detect Capital/mohaimen.cpp
+++ 520. Detect Capital/mohaimen.cpp
@@ -1,38 +1,78 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-      bool allupperCase = false;
-      bool alllowerCase = false;
-      bool normalCase = false;
-      if (word == "")
-          return false;
-      if (word.length() == 1)
-          return true;
-      for (int i = 0; i < word.length(); i++){
-        if (i == 0){
-            if (islower(word[i]))
-                alllowerCase = true;
-            else if (islower(word[i+1]))
-                normalCase = true;
-            else
-                allupperCase = true;
+        int i = word.length();
+        bool done = false;
+        bool allUpper = false;
+        bool allLower = false;
+        bool normal = false;
+        int begin = 0;
+        int end = i-1;
+        if (word == "")
+            return false;
+        if (i == 1)
+            return true;
+        if (islower(word[0])){
+            allLower = true;
         }
-        else if (alllowerCase){
-            if (!islower(word[i]))
-                return false;
-        }
-        else if (allupperCase){
-            if (islower(word[i]))
-                return false;
+        else if (!islower(word[0]) && islower(word[1])){
+            normal = true;
         }
         else{
-            if (i > 0){
-                if (!islower(word[i]))
+            allUpper = true;
+        }
+        while (!done){
+            if (allUpper){
+                if (isupper(word[begin]) && isupper(word[end])){
+                    begin ++;
+                    end --;
+                }
+                else
+                    return false;
+            }
+            else if (allLower){
+                if (islower(word[begin]) && islower(word[end])){
+                    begin ++;
+                    end --;
+                }
+                else
+                    return false;
+            }
+            else if (normal){
+                if (begin == 0){
+                    if (isupper(word[begin]) && islower(word[end])){
+                        begin ++;
+                        end --;
+                    }
+                    else
+                        return false;
+                }
+                else{
+                    if (islower(word[begin]) && islower(word[end])){
+                        begin ++;
+                        end --;
+                    }
+                    else
+                        return false;
+                }
+            }
+            if (begin > end)
+                done = true;
+            else if (begin == end){
+                if (allUpper){
+                    if (isupper(word[begin]))
+                        done = true;
+                    else return false;
+                }
+                else if (allLower || normal){
+                    if (islower(word[begin]))
+                        done = true;
+                    else return false;
+                }
+                else
                     return false;
             }
         }
-
-      }
         return true;
     }
 };
