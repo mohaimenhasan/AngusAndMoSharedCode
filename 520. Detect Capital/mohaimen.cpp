47789f2bb68259b81dfeabbc88efef8dbee7f1b1
Mohaimen Khan <mkhan@bluecatnetworks.com>
2018-07-30 23:56:33 -0400

    completed 520

--- 520. Detect Capital/mohaimen.cpp
+++ 520. Detect Capital/mohaimen.cpp
@@ -1,6 +1,38 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
+      bool allupperCase = false;
+      bool alllowerCase = false;
+      bool normalCase = false;
+      if (word == "")
+          return false;
+      if (word.length() == 1)
+          return true;
+      for (int i = 0; i < word.length(); i++){
+        if (i == 0){
+            if (islower(word[i]))
+                alllowerCase = true;
+            else if (islower(word[i+1]))
+                normalCase = true;
+            else
+                allupperCase = true;
+        }
+        else if (alllowerCase){
+            if (!islower(word[i]))
+                return false;
+        }
+        else if (allupperCase){
+            if (islower(word[i]))
+                return false;
+        }
+        else{
+            if (i > 0){
+                if (!islower(word[i]))
+                    return false;
+            }
+        }
 
+      }
+        return true;
     }
 };
