48ff8287c273219044a1d558d9b5cbcb90d4a645
Mohaimen Khan <mkhan@bluecatnetworks.com>
2018-08-11 11:57:41 -0400

    why hello there

---
+++ 378. Kth Smallest Element in a Sorted Matrix/mohaimen.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+      int n = matrix.size();
+      int iter = 0;
+      int row = 0;
+      int col = 0;
+      int smallest;
+      while (iter != k){
+        vector<int> temp;
+        temp = matrix[iter];
+
+      }
+    }
+};
