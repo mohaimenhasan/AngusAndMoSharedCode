bb8f5c1134a9348b34be631f07608919af935694
Mohaimen Khan <mkhan@bluecatnetworks.com>
2018-08-11 01:18:15 -0400

    Finished 328

--- 328. Odd Even Linked List/mohaimen.cpp
+++ 328. Odd Even Linked List/mohaimen.cpp
@@ -1,3 +1,4 @@
+/// TOOK ME TWO BILLION TRIES 
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -9,6 +10,25 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
+        if (head==NULL) return head;
+        if (head->next == NULL) return head;
+        if (head->next->next == NULL) return head;
+        ListNode* first = head;
+        ListNode* second = head->next;
+        ListNode* temp_first = first->next->next;
+        ListNode* temp_second = second->next->next;
+        first->next = temp_first;
+        second->next = temp_second;
+        if(temp_second != NULL){
+            while (temp_first->next != NULL && temp_second->next != NULL){
+                temp_first->next = temp_second->next;
+                temp_first = temp_first->next;
+                temp_second->next = temp_first->next;
+                temp_second = temp_second->next;
+            }
+        }
+        temp_first->next = second;
+        return head;
 
     }
 };
